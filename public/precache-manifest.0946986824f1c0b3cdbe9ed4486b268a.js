da68cc7193f91da9c3f350c3657345acfca7f15f
Ali Fadda <silvertechguy@gmail.com>
2020-10-20 04:59:04 +0200

    prepare to deploy

---
+++ public/precache-manifest.0946986824f1c0b3cdbe9ed4486b268a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26f4dff0c7dedf1a767fa725efa4e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db59b0b1a406e8b332cd",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fca6e0d5922f22e6d4a6",
+    "url": "/static/js/2.e3ea679e.chunk.js"
+  },
+  {
+    "revision": "bdf601f70d5cf39a852da643c2465654",
+    "url": "/static/js/2.e3ea679e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db59b0b1a406e8b332cd",
+    "url": "/static/js/main.735439f3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
